69c940e5c8b172d4ae79863e72d4a97c2a2b621c
Marquesjob <patrickmarques.job@hotmail.com>
2022-05-23 16:48:42 -0300

    path-dinamico

--- base/index.js
+++ base/index.js
@@ -1,6 +1,7 @@
-window.addEventListener("load", player.start());
+const path = (file) => {
+    return `files/${file}`
+}
 
-
-
+window.addEventListener('load', player.start());
 
 
--- base/player.js
+++ base/player.js
@@ -3,18 +3,15 @@ window.player = {
     title: document.querySelector(".card-content h5"),
     artist: document.querySelector(".artist"),
     audio: document.querySelector("audio"),
-    data: {
-        title:"Elevate!",
-        artist:"Frank Bentley",
-        gender: "hip-hop",
-        cover:"files/hiphop-music.jpg",
-        file:"files/elevate-by-frank-bentley.mp3"
-    },
-    start: function () {
-        this.cover.style.background = `url('${this.data.cover}') no-repeat center center / cover`
+    audioData: audios,
+    currentAudio: { },
+    start() {
+        this.currentAudio = this.audioData[0]
+
+        this.cover.style.background = `url('${path(this.currentAudio.cover)}') no-repeat center center / cover`
         this.cover.style.height = `40vh`
-        this.title.innerText = this.data.title;
-        this.artist.innerText = this.data.artist;
-        this.audio.src = this.data.file;
+        this.title.innerText = path(this.currentAudio.title);
+        this.artist.innerText = path(this.currentAudio.artist);
+        this.audio.src = path(this.currentAudio.file);
     }
 }
